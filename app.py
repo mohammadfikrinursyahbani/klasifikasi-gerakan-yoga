ad71ae19a82cb2600edd73c2618135db587c7dc3
Mohammad Fikri Nur Syahbani <72445001+mohammadfikrinursyahbani@users.noreply.github.com>
2023-01-30 07:55:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def load_model():
 model = load_model()
 
 st.title("Klasifikasi Gerakan Yoga")
+st.title("Klasifikasi Gerakan")
 
 file = st.file_uploader("Upload Gambar", type=["jpg", "jpeg", "png"])
 
